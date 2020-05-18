1ebe9b07952a7f2e7ba7dda3cfe197a22237bd6d
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-05-18 12:13:54 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Anil Kumar");
+$("#policyNumber").text("PL52071414");
+$("#startDate").text("19-May-2020");
+$("#endDate").text("18-May-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB08AA8760");
+$("#make").text("MARUTI");
+$("#model").text("MARUTI 800");
+$("#vehicalSubType").text("STD");
+$("#engineNo").text("1984093");
+$("#chassis").text("1409801");
